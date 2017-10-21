08b4bb9ca136142b08b4fdfa6868ab6864bd1856
Oliver Bristow <evilumbrella+github@gmail.com>
2017-10-21 15:31:52 +0100

    revert changes made in 6143c43cd3bd to ruby app as incredibly slow

--- servers/ruby_puma.rb
+++ servers/ruby_puma.rb
@@ -1,19 +1,15 @@
-require 'rack'
-require 'rack/handler/puma'
 # ruby actually sets TCP_CORK instead of TCP_NODELAY
 # https://github.com/puma/puma/blob/v3.10.0/lib/puma/server.rb#L100
+require 'rack/handler/puma'
 
-app = Rack::Builder.new do
-    @cache = {}
-    def call(env)
-        size = Integer(env['PATH_INFO'][1..-1]) rescue 1024
-        body = @cache[size]
-        if body.nil?
-            body = @cache[size] = 'X' * size
-        end
-        [200, {}, [body]]
+
+$cache = {}
+app = proc do |env|
+    size = Integer(env['PATH_INFO'][1..-1]) rescue 1024
+    if not $cache.key?(size)
+        $cache[size] = 'X' * size
     end
+    [200, {}, [$cache[size]]]
 end
 
-Rack::Server.start app: app, Port: 25000,
-    max_threads: 1, workers: 0, environment: 'none'
+Rack::Handler::Puma.run(app, Port: 25000, max_threads: 1, workers: 0, preload_app: true)
